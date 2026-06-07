d87c86bf95a9d2e6fff10f0995c83d10e7f04ded
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-07 15:09:26 +0200

    update app with routes and top nav in global context

--- ui/predictball/src/app/app.routes.ts
+++ ui/predictball/src/app/app.routes.ts
@@ -5,6 +5,8 @@ export const routes: Routes = [
   { path: 'home', loadComponent: () => import('./home-page/home.page').then(m => m.HomePage) },
   { path: 'login', loadComponent: () => import('./login-page/login.page').then(m => m.LoginPage) },
   { path: 'competition/:id', loadComponent: () => import('./competition-page/competition.page').then(m => m.CompetitionPage) },
+  { path: 'competitions', loadComponent: () => import('./competitions-page/competitions-page').then(m => m.CompetitionsPage) },
   { path: 'profile', loadComponent: () => import('./profile-page/profile.page').then(m => m.ProfilePage) },
+  { path: 'about', loadComponent: () => import('./about-page/about-page').then(m => m.AboutPage) },
   { path: '**', redirectTo: 'home' }
 ];
--- ui/predictball/src/app/app.ts
+++ ui/predictball/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopNavigation } from './top-navigation/top-navigation';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopNavigation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
