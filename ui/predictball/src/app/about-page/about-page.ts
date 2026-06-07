f16d3ce0be0279cbc64ddd0d143de4164b0fe472
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-07 19:29:28 +0200

    add about-page

--- ui/predictball/src/app/about-page/about-page.ts
+++ ui/predictball/src/app/about-page/about-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about-page',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './about-page.html',
   styleUrl: './about-page.css',
 })
