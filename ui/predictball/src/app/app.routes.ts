fee6248bd84c66399b5a4ae98d5ae3b4c5422995
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-07 05:23:29 +0200

    root adjustments in config and routes

--- ui/predictball/src/app/app.routes.ts
+++ ui/predictball/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // Lazy load standalone components for better performance
+  { path: 'home', loadComponent: () => import('./home.page/home.page').then(m => m.HomePage) },
+  { path: 'login', loadComponent: () => import('./login.page/login.page').then(m => m.LoginPage) },
+  { path: 'competition/:id', loadComponent: () => import('./competition.page/competition.page').then(m => m.CompetitionPage) },
+  { path: 'profile', loadComponent: () => import('./profile.page/profile.page').then(m => m.ProfilePage) },
+  { path: '**', redirectTo: 'home' }
+];
