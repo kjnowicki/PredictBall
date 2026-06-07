8790c65a762a4ef47ee67d1bcc0279f94c1add60
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-07 15:46:56 +0200

    top-navigation using material

--- ui/predictball/src/app/top-navigation/top-navigation.ts
+++ ui/predictball/src/app/top-navigation/top-navigation.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-top-navigation',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatTabsModule],
   templateUrl: './top-navigation.html',
   styleUrl: './top-navigation.css',
 })
