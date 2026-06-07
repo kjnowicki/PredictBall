eb85c80653c00fed823c3a0137a6f2e4c69763cb
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-07 13:12:42 +0200

    fix routing, remove template

--- ui/predictball/src/main.server.ts
+++ ui/predictball/src/main.server.ts
@@ -1,7 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(App, config);
-
-export default bootstrap;
+export default (context: BootstrapContext) =>
+  bootstrapApplication(App, config, context);
