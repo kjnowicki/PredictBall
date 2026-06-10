44c65a34f7cb59dd6a0ea3e277aa5357fad63b23
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-10 04:09:56 +0200

    production-ready handler

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -274,7 +274,7 @@ func (h *APIHandler) HandleGetTeamDetails(w http.ResponseWriter, r *http.Request
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+		w.Header().Set("Access-Control-Allow-Origin", "https://predict-ball.eu")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
