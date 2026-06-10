f63816aae6748bb04bfe5c0aa8ad0d14b1a03715
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-10 05:08:17 +0200

    possibly fix CORS via differnt cors middleware

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -276,12 +276,12 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "https://predict-ball.eu")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Origin")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		// Handle preflight requests, which are sent by the browser before the actual request.
 		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusNoContent)
+			w.WriteHeader(http.StatusOK)
 			return
 		}
 
