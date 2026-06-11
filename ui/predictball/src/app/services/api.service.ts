a24c85a3252e203221bac454377236e9328089d6
Krzysztof Nowicki <nowicki.krzysztof.jan@gmail.com>
2026-06-11 05:53:13 +0200

    update env files logic for ui

--- ui/predictball/src/app/services/api.service.ts
+++ ui/predictball/src/app/services/api.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   private http = inject(HttpClient);
-  private baseUrl = 'http://localhost:8080'; 
+  private baseUrl = environment.apiUrl; 
 
   get<T>(endpoint: string, queryParams?: Record<string, string | number | boolean>): Observable<T> {
     let params = new HttpParams();
